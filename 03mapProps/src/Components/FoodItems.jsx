dc6f81eed83869025c73528d506f48ddd13aa472
Your Name <sharmaanshul9796@gmail.com>
2023-11-27 00:11:06 +0530

    styles.module.css

--- 03mapProps/src/Components/FoodItems.jsx
+++ 03mapProps/src/Components/FoodItems.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import Item from './Item'
+import styles from './FoodItem.module.css'
 
 function FoodItems({foodItems}) {
   return (
     <>
      
-      <ul className="list-group">
+      <ul className={`${styles["food-items"]} list-group`}>
         {foodItems.map((item) => (
           <Item key={item} item = {item}/>
         ))}
--- 03mapProps/src/Components/Item.jsx
+++ 03mapProps/src/Components/Item.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import styles from './Item.module.css'
 
 function Item({item}) {
   return (
     <>
-      <li className="list-group-item">
+      <li className= {`${styles["item-color"]} list-group-item`}>
         {item}
       </li>
     </>
