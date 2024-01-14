e369c50d39da785c402f9472f869aa49464c00d5
Your Name <sharmaanshul9796@gmail.com>
2024-01-14 22:49:12 +0530

    kl

--- 11-MyntraClone/3-myntra-react-clone/src/store/fetchStatusSlice.js
+++ 11-MyntraClone/3-myntra-react-clone/src/store/fetchStatusSlice.js
@@ -2,23 +2,20 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const fetchStatusSlice = createSlice({
   name: "fetchStatus",
-  initialState: 
-    {
-      fetchDone: false,
-      currentlyFetching: false,
-    },
-  
+  initialState: {
+    fetchDone: false,
+    currentlyFetching: false,
+  },
   reducers: {
     markFetchDone: (state) => {
-       state.fetchDone = true;
-      // state[0].fetchDone = true;
+      state.fetchDone = true;
     },
-    fetching : (state) => {
-     state.currentlyFetching = true;
+    fetching: (state) => {
+      state.currentlyFetching = true;
     },
     fetchingDone: (state) => {
-     state.currentlyFetching = false;
-    }
+      state.currentlyFetching = false;
+    },
   },
 });
 
